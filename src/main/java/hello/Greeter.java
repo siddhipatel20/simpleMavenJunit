f87b155cfac14eeb009f4163d78737da5f9d9067
ashutoshbhakare <bhakareashutosh@yahoo.com>
2022-07-23 01:00:07 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world WEBBBBBB HOOOOOOOKKKKKKKSSSSSS- This is Pavan Wankhade your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
+    return "Hello world WEBBBBBB HOOOOOOOKKKKKKKSSSSSS SIIIIIIIIIIIIMMMMM- This is Pavan Wankhade your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
   }
 }
